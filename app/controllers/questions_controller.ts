3356ed4c7df4e72d3ef268f1bb61656f6440b543
Iyan Zuli Armanda <catcode0101@gmail.com>
2025-07-17 17:04:23 +0700

    feat: add new API endpoint to send question to chatbot, save question and answer, and return chatbot response to user

---
+++ app/controllers/questions_controller.ts
@@ -0,0 +1,63 @@
+import type { HttpContext } from '@adonisjs/core/http'
+import { v4 as uuidv4 } from 'uuid'
+import axios from 'axios'
+
+import Conversation from '#models/conversation'
+import Message from '#models/message'
+
+export default class QuestionsController {
+  public async store({ request, response }: HttpContext) {
+    const question = request.input('question')
+
+    if (!question) {
+      return response.badRequest({ error: 'Pertanyaan tidak boleh kosong' })
+    }
+
+    // Generate session_id UUID
+    const sessionId = uuidv4()
+
+    try {
+      // Simpan pesan dari user
+      const conversation = await Conversation.create({ sessionId })
+
+      const userMessage = await Message.create({
+        conversationId: conversation.id,
+        senderType: 'user',
+        message: question,
+      })
+
+      // Kirim ke API eksternal
+      const apiResponse = await axios.post(
+        'https://api.majadigidev.jatimprov.go.id/api/external/chatbot/send-message',
+        {
+          question: question,
+          additional_context: '',
+          session_id: sessionId,
+        }
+      )
+
+      const botMessageText = apiResponse.data?.data?.message?.[0]?.text ?? 'Jawaban tidak tersedia'
+
+      // Simpan pesan dari bot
+      const botMessage = await Message.create({
+        conversationId: conversation.id,
+        senderType: 'bot',
+        message: botMessageText,
+      })
+
+      // Update last_message_id di conversation
+      conversation.lastMessageId = botMessage.id
+      await conversation.save()
+
+      // Mengembalikan respon ke user
+      return response.created({
+        session_id: sessionId,
+        question: userMessage.message,
+        answer: botMessage.message,
+      })
+    } catch (error) {
+      console.error('error: ', error)
+      return response.status(500).json({ error: 'Gagal memproses pertanyaa' })
+    }
+  }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -9,8 +9,12 @@
 
 import router from '@adonisjs/core/services/router'
 
+const QuestionsController = () => import('#controllers/questions_controller')
+
 router.get('/', async () => {
   return {
     hello: 'world',
   }
 })
+
+router.post('/questions', [QuestionsController, 'store'])
